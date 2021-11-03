058ef07060ba60f0a33c7ffd91d6c00537f63155
NAT <ntuan.2502@gmail.com>
2021-11-03 16:55:41 +0700

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0d9412fe729f5e25f444ddf830b71dad'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'fad7bd84-dbab-4718-8942-0ede382e6b0a'
+};
