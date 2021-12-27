723d5a67d847dc0d61f0389701ab490ff10f28d7
NAT <ntuan.2502@gmail.com>
2021-12-27 16:18:42 +0700

    add env url

--- config/server.js
+++ config/server.js
@@ -1,9 +1,10 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: env("URL", "http://localhost:1337"),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '0d9412fe729f5e25f444ddf830b71dad'),
+      secret: env("ADMIN_JWT_SECRET", "0d9412fe729f5e25f444ddf830b71dad"),
     },
   },
 });
